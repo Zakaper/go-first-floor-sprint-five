02489b4221b386f7feef0a6faea7f65a0a2a778e
Zakaper <zakabuninpeter@gmail.com>
2025-01-08 02:26:09 +0500

    fourth commit

--- ftracker.go
+++ ftracker.go
@@ -104,7 +104,7 @@ const (
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
 	height = height / 100
-	averageSpeedWalking := meanSpeed(action, duration)
+	averageSpeedWalking := meanSpeed(action, duration) * kmhInMsec
 	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(averageSpeedWalking, 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
 }
 
